8e6a7e14303ebbc80a0db43c7c796b8fab781417
BunnyNabbit <60326111+BunnyNabbit@users.noreply.github.com>
2023-11-21 21:01:27 -0600

    Create minimum.js

---
+++ examples/minimum.js
@@ -0,0 +1,13 @@
+// Bare minimum server operation
+const Server = require("classicborne-server-protocol")
+const server = new Server(25565)
+
+const bounds = { x: 512, y: 512, z: 512 }
+const emptyLevelBuffer = server.utils.processLevel(Buffer.alloc(bounds.x * bounds.y * bounds.z), bounds.x, bounds.y, bounds.z)
+
+server.on("clientConnected", async (client, authInfo) => {
+	console.log(emptyLevelBuffer)
+	client.serverIdentification("Empty", "So cool!", 0x00)
+	client.loadLevel(await emptyLevelBuffer, bounds.x, bounds.y, bounds.z, true)
+	client.configureSpawn(0, authInfo.username, 0, 0, 0, 0, 0)
+})
