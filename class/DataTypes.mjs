acc2e33bd8f650fd60b3d9eef9749cb79e8cb4c2
BunnyNabbit (Aon) <60326111+BunnyNabbit@users.noreply.github.com>
2025-12-06 01:39:13 -0600

    Add type comments to DataTypes.mjs

--- class/DataTypes.mjs
+++ class/DataTypes.mjs
@@ -1,14 +1,21 @@
+// @ts-check
 import { SmartBuffer } from "smart-buffer"
 import { CodePage437 } from "./CodePage437.mjs"
 /**Namespace for data type handling
  * @namespace
  */
 export class DataTypes {
-	/** */
+	/**@todo Yet to be documented.
+	 * @param {SmartBuffer} buffer 
+	 * @returns 
+	 */
 	static readString(buffer) {
 		return CodePage437.from(buffer.readBuffer(64)).trim()
 	}
-
+	/**@todo Yet to be documented.
+	 * @param {SmartBuffer} buffer 
+	 * @returns 
+	 */
 	static readFixedShort(buffer) {
 		const data = buffer.readUInt16BE()
 		if (data & 0x8000) {
@@ -17,14 +24,20 @@ export class DataTypes {
 			return data / 32
 		}
 	}
-
-	static fixedShort(num) {
-		let multipliedValue = Math.round(num * 32)
+	/**@todo Yet to be documented.
+	 * @param {number} number 
+	 * @returns 
+	 */
+	static fixedShort(number) {
+		let multipliedValue = Math.round(number * 32)
 		let bitwiseResult = multipliedValue & 0b111111111111111
 		if (Math.sign(multipliedValue) == -1) bitwiseResult |= 0b1000000000000000
 		return bitwiseResult
 	}
-
+	/**@todo Yet to be documented.
+	 * @param {string} string 
+	 * @returns 
+	 */
 	static padString(string) {
 		const buffer = new SmartBuffer({ size: 64 })
 		buffer.writeBuffer(CodePage437.to(string))
