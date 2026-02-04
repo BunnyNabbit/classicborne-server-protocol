a154fe193b1e10ceda0ac984d6370f2ab474ade3
BunnyNabbit (Aon) <60326111+BunnyNabbit@users.noreply.github.com>
2026-02-04 14:46:44 -0600

    Use italics when referring to packages in documentation

--- class/ExtensionManager.mjs
+++ class/ExtensionManager.mjs
@@ -6,7 +6,7 @@
 
 /**I manage the currently applied Classic Protocol Extension extensions for a {@link Client}.
  *
- * In the `classicborne` ecosystem, clear communication is valued. This means that some extensions have the same name as described in their Classic Protocol Extension. However, some are either unclear or misleading, so they may be referred to as differently in `classicborne-server-protocol`. This is of course no exception when retriving extensions from me. See the {@link extensions} namespace for what these extensions are called.
+ * In the *classicborne* ecosystem, clear communication is valued. This means that some extensions have the same name as described in their Classic Protocol Extension. However, some are either unclear or misleading, so they may be referred to as differently in *classicborne-server-protocol*. This is of course no exception when retriving extensions from me. See the {@link extensions} namespace for what these extensions are called.
  *
  * @extends {Map<keyof defaultExtensions, BaseExtension>}
  */
