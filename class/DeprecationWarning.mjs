3c1f4283adcfaf4ded1dc3c41979063f8da0c150
BunnyNabbit (Aon) <60326111+BunnyNabbit@users.noreply.github.com>
2026-02-04 14:46:15 -0600

    Document DeprecationWarning#warn

--- class/DeprecationWarning.mjs
+++ class/DeprecationWarning.mjs
@@ -1,6 +1,10 @@
 export class DeprecationWarning {
 	static warnedIds = new Set()
-	/**/
+	/**Sends a {@link uniqueId | unique} {@link message | warning} to the console. Uses {@link warnedIds} for checking duplicates and adding warned {@link uniqueId} values.
+	 *
+	 * @param {any} uniqueId - The unique identifier of the message.
+	 * @param {string} message - The message to display.
+	 */
 	static warn(uniqueId, message) {
 		if (this.warnedIds.has(uniqueId)) return
 		this.warnedIds.add(uniqueId)
