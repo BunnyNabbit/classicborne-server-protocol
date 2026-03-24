4439b7640f826e4661e8834f09e624133b0d990e
BunnyNabbit “Aon” <aon@bunnynabbit.com>
2026-03-23 21:14:49 -0500

    Format and fix typo in CpeBase comments

--- class/extensions/CpeBase.mjs
+++ class/extensions/CpeBase.mjs
@@ -4,12 +4,10 @@ import { ProtocolExtensionIdentify } from "./packetHandlers/ProtocolExtensionIde
 import { DefineProtocolExtension } from "./packetHandlers/DefineProtocolExtension.mjs"
 /** @import {Client} from "../Client.mjs" */
 
-/** I am a psuedo-extension. While I am not truly a CPE extension, I exist to provide communication for initial CPE client connections. Because of this, a client isn't able to request this extension, only indirectly. Actually, I am not even added to the client's extension manager. My constructor is only used to provide packet handlers. */
+/** I am a pseudo-extension. While I am not truly a CPE extension, I exist to provide communication for initial CPE client connections. Because of this, a client isn't able to request this extension, only indirectly. Actually, I am not even added to the client's extension manager. My constructor is only used to provide packet handlers. */
 export class CpeBase extends BaseExtension {
-	/**@param {Client} client
-	 * @param {number} version
-	 */
-	constructor(client, version) {
+	/** @param {Client} client */
+	constructor(client) {
 		super(client, 1)
 		client.packetHandlers.set(ProtocolExtensionIdentify.packetId, new ProtocolExtensionIdentify(client))
 		client.packetHandlers.set(DefineProtocolExtension.packetId, new DefineProtocolExtension(client))
